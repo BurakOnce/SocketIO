9bf3eab7a027c642f0892dc93dfb067501af7ab2
burakonce9 <burakonce9@gmail.com>
2024-09-18 15:06:41 +0300

    Add project files.

---
+++ ClientIO/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientIO
+{
+    internal class Program
+    {
+        private const int Port = 1980;
+        private const string IpAddress = "127.0.0.1";
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Type 'exit' to close the client.");
+                bool done = false;
+
+                while (!done)
+                {
+                    Console.WriteLine("Your message for server:");
+                    string message = Console.ReadLine();
+
+                    if (message.ToLower() == "exit")
+                    {
+                        done = true;
+                        break;
+                    }
+
+                    if (!string.IsNullOrEmpty(message))
+                    {
+                        TcpClient client = new TcpClient(IpAddress, Port);
+
+                        byte[] sendData = Encoding.UTF8.GetBytes(message);
+
+                        using (NetworkStream networkStream = client.GetStream())
+                        {
+                            await networkStream.WriteAsync(sendData, 0, sendData.Length);
+                        }
+
+                        client.Close();
+                        Console.WriteLine("Message sent.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Empty message cannot be sent.");
+                    }
+                }
+                Console.WriteLine("Client closed.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while connecting to the device: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ ServerIO/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerIO
+{
+    class Program
+    {
+        private const int Port = 1980; 
+
+        static async Task Main(string[] args)
+        {
+            var server = new TcpListener(IPAddress.Any, Port);
+            server.Start();
+            Console.WriteLine("Server listening started.");
+
+            bool loggedNoRequest = false;
+            bool done = false;
+
+            while (!done)
+            {
+                if (!server.Pending())
+                {
+                    if (!loggedNoRequest)
+                    {
+                        Console.WriteLine("There are no pending requests yet.");
+                        loggedNoRequest = true;
+                    }
+                    await Task.Delay(1000); 
+                }
+                else
+                {
+                    loggedNoRequest = false;
+
+                    var client = await server.AcceptTcpClientAsync();
+                    Console.WriteLine("\n===== Client connection received =====");
+
+                    var stream = client.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                    if (bytesRead > 0)
+                    {
+                        var requestMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                        Console.ForegroundColor = ConsoleColor.Green; 
+                        Console.WriteLine("\n**** Client Message Received ****");
+                        Console.WriteLine(requestMessage);
+                        Console.WriteLine("================================\n");
+                        Console.ResetColor();
+
+                        string data = new string(requestMessage.Where(c => !char.IsControl(c)).ToArray());
+                        Console.WriteLine("Processed Message: " + data);
+                    }
+
+                    client.Close();
+                    Console.WriteLine("Client connection closed.");
+                }
+            }
+
+            server.Stop();
+            Console.WriteLine("Server stoped.");
+        }
+    }
+}
